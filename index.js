95cb7b7bb55b76b32b21b9de906b7591a97c35ce
Andrey <antonov.andrey.10101@gmail.com>
2023-05-12 18:38:46 +0300

    feat: init project

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import multer from "multer";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("DB Ok");
+    })
+    .catch((err) => console.log("DB error", err));
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(process.env.PORT || 4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("Server Ok");
+});
