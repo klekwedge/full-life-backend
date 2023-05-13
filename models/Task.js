4ea838f4d8dd83137ec78b4239eb2f649b3ec8c3
Andrey <antonov.andrey.10101@gmail.com>
2023-05-13 17:49:09 +0300

    feat: add Task model

---
+++ models/Task.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        text: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        tags: {
+            type: Array,
+            default: [],
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        imageUrl: String,
+    }
+    ,
+    {
+        timestamps: true,
+    }
+)
+
+export default mongoose.model("Post", PostSchema);
